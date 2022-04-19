603830d641a90e9495812706dd2c32ff269bd3ef
ACompleteNoobSmoke <OmoruyiOmofonmwan@yahoo.com>
2022-04-19 06:04:18 -0500

    Worked on Lifecycle

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import logo from './logo.svg';
 import Greet from './components/Greet';
 import './App.css';
 import Form from './components/Form';
+import LifecycleA from './components/LifecycleA';
 
 
 function App() {
   return (
     <div className="App">
-      <Form />
+     <LifecycleA />
     </div>
   );
 }
